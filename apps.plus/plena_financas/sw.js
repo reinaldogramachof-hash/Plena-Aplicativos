0c338d0cdd0156c95475a11703b6462a06df8f8b
reinaldogramachof-hash <reinaldogramachof@gmail.com>
2026-01-06 16:03:13 -0300

    limpeza tree

---
+++ apps.plus/plena_financas/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'plena-financas-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
---
+++ apps.plus/plena_pdv/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'plena-pdv-v1';
+const ASSETS = [
+    './index.html',
+    './manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(response => response || fetch(e.request))
+    );
+});
